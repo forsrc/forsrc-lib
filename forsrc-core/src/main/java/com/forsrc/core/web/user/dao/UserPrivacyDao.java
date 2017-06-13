619085bfe73d5fde61260d193f360b1bd995d9d8
forsrc <forsrc@163.com>
2017-06-13 10:03:05 +0900

    dao

--- forsrc-core/src/main/java/com/forsrc/core/web/user/dao/UserPrivacyDao.java
+++ forsrc-core/src/main/java/com/forsrc/core/web/user/dao/UserPrivacyDao.java
@@ -1,13 +1,11 @@
 package com.forsrc.core.web.user.dao;
 
-import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.stereotype.Repository;
 
 import com.forsrc.core.base.dao.BaseDao;
 import com.forsrc.pojo.UserPrivacy;
 
 @Repository
-@CacheConfig(cacheNames = "ehcache_pojo")
 public interface UserPrivacyDao extends BaseDao<UserPrivacy, Long> {
     UserPrivacy findByUsername(String username);
 }
--- forsrc-core/src/main/java/com/forsrc/core/web/user/dao/UserRoleDao.java
+++ forsrc-core/src/main/java/com/forsrc/core/web/user/dao/UserRoleDao.java
@@ -2,14 +2,12 @@ package com.forsrc.core.web.user.dao;
 
 import java.util.List;
 
-import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.stereotype.Repository;
 
 import com.forsrc.core.base.dao.BaseDao;
 import com.forsrc.pojo.UserRole;
 
 @Repository
-@CacheConfig(cacheNames = "ehcache_pojo")
 public interface UserRoleDao extends BaseDao<UserRole, Long> {
 
     List<UserRole> findByUserId(Long userId);
