c660b8e3de601887264241f1de4835d7acdfe1d6
yes <forsrc@163.com>
2017-08-09 12:51:58 +0900

    Update TimeoutUtils.java

--- forsrc-utils/src/main/java/com/forsrc/utils/TimeoutUtils.java
+++ forsrc-utils/src/main/java/com/forsrc/utils/TimeoutUtils.java
@@ -28,26 +28,118 @@ public class TimeoutUtils {
         }
     }
 
-    public static void main(String[] args) throws TimeoutException, InterruptedException, ExecutionException {
-        Long sum = TimeoutUtils.timeout(1000, TimeUnit.MILLISECONDS, new Callable<Long>() {
-
+    public static void timeout(final Long time, Runnable runnable) {
+        final CyclicBarrier cyclicBarrier = new CyclicBarrier(2);
+        final Thread targetThread = new Thread(runnable);
+        Runnable interrupterRunbable = new Runnable() {
             @Override
-            public Long call() throws Exception {
+            public void run() {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(time);
+                    // targetThread.stop();
+                    if (!targetThread.isInterrupted()) {
+                        targetThread.interrupt();
+                    }
+                } catch (InterruptedException e) {
+                    // e.printStackTrace();
+                    // System.out.println("Not timeout!");
+                }
+                try {
+                    cyclicBarrier.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        Thread interrupterThread = new Thread(interrupterRunbable);
+        targetThread.start();
+        interrupterThread.start();
+        try {
+            targetThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        interrupterThread.interrupt();
+        try {
+            cyclicBarrier.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+    }
 
-                return System.currentTimeMillis();
+    public static void timeout(final Long time, Handler<Void> handler) {
+
+        timeout(1000L, new Runnable() {
+            @Override
+            public void run() {
+                handler.handle();
             }
         });
+    }
+
+    public static abstract class Handler<T> {
+        public abstract T handle();
+
+        public boolean isInterrupted() {
+            try {
+                TimeUnit.MICROSECONDS.sleep(1);
+                return false;
+            } catch (InterruptedException e) {
+                return true;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Long sum = 0L;
+        try {
+            sum = TimeoutUtils.timeout(1000, TimeUnit.MILLISECONDS, new Callable<Long>() {
+
+                @Override
+                public Long call() throws Exception {
+
+                    return System.currentTimeMillis();
+                }
+            });
+        } catch (TimeoutException | InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
         System.out.println(sum);
 
-        sum = TimeoutUtils.timeout(1000, TimeUnit.MILLISECONDS, new Callable<Long>() {
+        try {
+            sum = TimeoutUtils.timeout(1000, TimeUnit.MILLISECONDS, new Callable<Long>() {
 
+                @Override
+                public Long call() throws Exception {
+                    TimeUnit.MILLISECONDS.sleep(1500);
+                    return System.currentTimeMillis();
+                }
+            });
+        } catch (TimeoutException | InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        System.out.println(sum);
+
+        timeout(1000L, new Runnable() {
             @Override
-            public Long call() throws Exception {
-                TimeUnit.MILLISECONDS.sleep(1500);
-                return System.currentTimeMillis();
+            public void run() {
+                System.out.println("-->" + System.currentTimeMillis());
+                try {
+                    for (int i = 0; i < 1000000000; i++) {
+                        TimeUnit.MICROSECONDS.sleep(1);
+                    }
+                    System.out.println("-->" + System.currentTimeMillis());
+                    System.out.println("OK");
+                } catch (Exception e) {
+                    // e.printStackTrace();
+                    System.out.println("-->" + System.currentTimeMillis());
+                    System.out.println("Timeout!");
+                }
             }
         });
-        System.out.println(sum);
-
     }
 }
